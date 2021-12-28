3c24ac9d227cf848293124559f74f1c36d3b34b4
onur-akdogan <onurakdogan7106@gmail.com>
2021-12-28 16:09:04 +0300

    Update index.blade.php

--- resources/views/backend/notification/index.blade.php
+++ resources/views/backend/notification/index.blade.php
@@ -11,7 +11,7 @@
             <div class="form-group">
                 <label>Başlık</label>
                 <div class="row">
-                    <div class="col-xs-12">
+                    <div class="col-lg-12">
                         <input type="text" name="title" required="" class="form-control">
                     </div>
                 </div>
@@ -19,7 +19,7 @@
             <div class="form-group">
                 <label>Resim Url</label>
                 <div class="row">
-                    <div class="col-xs-12">
+                    <div class="col-lg-12">
                         <input type="text" name="image"  class="form-control">
                     </div>
                 </div>
@@ -27,7 +27,7 @@
             <div class="form-group">
                 <label>Bildirim İçeriği</label>
                 <div class="row">
-                    <div class="col-xs-12">
+                    <div class="col-lg-12">
                         <input type="text" name="body" required="" class="form-control">
                     </div>
                 </div>
@@ -35,8 +35,8 @@
             <div class="form-group">
                 <label>Bildirim Türü</label>
                 <div class="row">
-                    <div class="col-xs-12">
-                        <select name="bildirim_turu" id="">
+                    <div class="col-lg-12">
+                        <select name="bildirim_turu " class="col-lg-12" id="">
                             <option value="">Seçiniz</option>
 
                             <option value="haber_id">Haber</option>
@@ -48,7 +48,7 @@
             <div class="form-group">
                 <label>Haber Id Giriniz</label>
                 <div class="row">
-                    <div class="col-xs-12">
+                    <div class="col-lg-12">
                         <input type="text" name="id"  class="form-control">
                     </div>
                 </div>
