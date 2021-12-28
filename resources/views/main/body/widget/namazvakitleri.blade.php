9209793974ffc6f61c8395145d2954d6920c967e
obabeyi <43567012+obabeyi@users.noreply.github.com>
2021-12-28 14:20:30 +0300

    Update namazvakitleri.blade.php

--- resources/views/main/body/widget/namazvakitleri.blade.php
+++ resources/views/main/body/widget/namazvakitleri.blade.php
@@ -25,23 +25,23 @@
 
                     <div
                         class="col-lg-2 col-md-2 col-sm-2 col-6  padding-left mt-1 namaz__kolon text-center font-weight-bold">
-                        <img src="img/imsak.png" width="30" class="img-fluid mr-2" alt=""><span
+                        <img src="{{asset('storage/imsak.png')}}" width="30" class="img-fluid mr-2" alt=""><span
                             class="text-light mb-0  namaz__kolon-text">İmsak {{$vakitler['imsak']}}</span></div>
                     <div
                         class="col-lg-2 col-md-2 col-sm-2 col-6  padding-left mt-1 namaz__kolon text-center font-weight-bold">
-                        <img src="img/ogle.png" width="30" class="img-fluid mr-2" alt=""><span
+                        <img src="{{asset('storage/ogle.png')}}" width="30" class="img-fluid mr-2" alt=""><span
                             class="text-light mb-0  namaz__kolon-text">Öğle {{$vakitler["ogle"]}}</span></div>
                     <div
                         class="col-lg-2 col-md-2 col-sm-2 col-6  padding-left mt-1 namaz__kolon text-center font-weight-bold">
-                        <img src="img/ikindi.png" width="30" class="img-fluid mr-2" alt=""><span
+                        <img src="{{asset('storage/ikindi.png')}}" width="30" class="img-fluid mr-2" alt=""><span
                             class="text-light mb-0  namaz__kolon-text">İkindi {{$vakitler["ikindi"]}}</span></div>
                     <div
                         class="col-lg-2 col-md-2 col-sm-2 col-6  padding-left mt-1 namaz__kolon text-center font-weight-bold">
-                        <img src="img/aksam.png" width="30" class="img-fluid mr-2" alt=""><span
+                        <img src="{{asset('storage/aksam.png')}}" width="30" class="img-fluid mr-2" alt=""><span
                             class="text-light mb-0  namaz__kolon-text">Akşam {{$vakitler["aksam"]}}</span></div>
                     <div
                         class="col-lg-2 col-md-2 col-sm-2 col-6  padding-left mt-1 namaz__kolon text-center font-weight-bold">
-                        <img src="img/yatsi.png" width="30" class="img-fluid mr-2" alt=""><span
+                        <img src="{{asset('storage/yatsi.png')}}" width="30" class="img-fluid mr-2" alt=""><span
                             class="text-light mb-0  namaz__kolon-text">Yatsı {{$vakitler["yatsi"]}}</span></div>
 
 
