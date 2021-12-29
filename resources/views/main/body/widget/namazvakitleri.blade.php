3bff8c414f32b40a71fd95ae9d261585a276c9ae
obabeyi <43567012+obabeyi@users.noreply.github.com>
2021-12-29 11:55:06 +0300

    ++

--- resources/views/main/body/widget/namazvakitleri.blade.php
+++ resources/views/main/body/widget/namazvakitleri.blade.php
@@ -44,6 +44,70 @@
                         class="col-lg-2 col-md-2 col-sm-2 col-6  padding-left mt-1 namaz__kolon text-center font-weight-bold">
                         <img src="{{asset('image/yatsi.png')}}" width="30" class="img-fluid mr-2" alt=""><span
                             class="text-light mb-0  namaz__kolon-text">Yatsı {{$vakitler["yatsi"]}}</span></div>
+                    <div class="col-md-2 col-5 my-auto text-success text-center" style="font-size: 13px">
+                        @php $now = Carbon\Carbon::now()->format('H:i');
+                  $imsak = $vakitler["imsak"];
+                                $gunes = $vakitler['gunes'];
+                                $ogle = $vakitler['ogle'];
+                                $ikindi = $vakitler['ikindi'];
+                                $aksam = $vakitler['aksam'];
+                                $yatsi = $vakitler['yatsi'];
+                        @endphp
+                        @if($now < $imsak )
+                            @php $startTime = Carbon\Carbon::parse($now);
+                    $finishTime = Carbon\Carbon::parse($gunes);
+                $totalDuration = $finishTime->diff($startTime)->format('%H:%i');
+                            @endphp
+
+                            <div class="kalansure">
+                                <span>{{ $totalDuration}}</span>
+                                <p> İmsak'a Kalan Süre</p>
+                            </div>
+
+
+                        @elseif($now<$ogle )
+                            @php $startTime = Carbon\Carbon::parse($now);
+                    $finishTime = Carbon\Carbon::parse($ogle);
+                $totalDuration = $finishTime->diff($startTime)->format('%H:%i');
+                            @endphp
+
+                            <div class="kalansure">
+                                <span>{{ $totalDuration}}</span>
+                                <p> Öğleye kalan Süre</p>
+                            </div>
+                        @elseif($now<$ikindi)
+                            @php $startTime = Carbon\Carbon::parse($now);
+                    $finishTime = Carbon\Carbon::parse($ikindi);
+                $totalDuration = $finishTime->diff($startTime)->format('%H:%i');
+                            @endphp
+                            {{--                    <span style="    font-size: 16px;font-weight: 700;color: #006726;letter-spacing: .25px;padding: 5px 6px;background: #e6f0e7;display: block;position: relative;">{{ $totalDuration}}</span>--}}
+                            <div class="kalansure">
+                                <span>{{ $totalDuration}}</span>
+                                <p>İkindi'ye Kalan Süre</p>
+                            </div>
+                        @elseif ($now<$aksam )
+                            @php $startTime = Carbon\Carbon::parse($now);
+                    $finishTime = Carbon\Carbon::parse($aksam);
+                $totalDuration = $finishTime->diff($startTime)->format('%H:%i');
+                            @endphp
+                            <div class="kalansure">
+                                <span>{{ $totalDuration}}</span>
+                                <p>Akşam'a Kalan Süre</p>
+
+                            </div>
+                        @elseif($now<$yatsi )
+                            @php $startTime = Carbon\Carbon::parse($now);
+                    $finishTime = Carbon\Carbon::parse($yatsi);
+                $totalDuration = $finishTime->diff($startTime)->format('%H:%i');
+                            @endphp
+                            <div class="kalansure">
+                                <span>{{ $totalDuration}}</span>
+                                <p>Yatsıy'a Kalan Süre</p>
+                            </div>
+
+
+                        @endif
+                    </div>
 
 
                 </div>
