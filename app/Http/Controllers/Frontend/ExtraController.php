9c8d338559937402e9fef22b50d3e75474e3f672
onur-akdogan <onurakdogan7106@gmail.com>
2022-01-08 16:38:25 +0300

    Update ExtraController.php

--- app/Http/Controllers/Frontend/ExtraController.php
+++ app/Http/Controllers/Frontend/ExtraController.php
@@ -852,8 +852,9 @@ class ExtraController extends Controller
     public function search(Request $request)
     {
         $searchText = $request['searchtext'];
-        $json = Post::orWhere('title_tr', 'LIKE', '%' . $searchText . '%')->orWhere('title_en', 'LIKE', '%' . $searchText . '%')->orWhere('subtitle_tr', 'LIKE', '%' . $searchText . '%')->orWhere('subtitle_en', 'LIKE', '%' . $searchText . '%')->where('status','=',1)->get();
+        $json = Post::orWhere('title_tr', 'LIKE', '%' . $searchText . '%')->orWhere('title_en', 'LIKE', '%' . $searchText . '%')->orWhere('subtitle_tr', 'LIKE', '%' . $searchText . '%')->orWhere('subtitle_en', 'LIKE', '%' . $searchText . '%')->where('status',1)->get();
         $searchNews = $this->change($json);
+        dd($searchNews);
         return \view('main.body.search', compact('searchNews'));
     }
 
