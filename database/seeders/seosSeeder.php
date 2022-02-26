18feb2e739a8d50809fa42747be7e14b5dde5f71
onur-akdogan <onurakdogan7106@gmail.com>
2022-02-26 10:16:15 +0300

    Add fcm Key

--- database/seeders/seosSeeder.php
+++ database/seeders/seosSeeder.php
@@ -15,17 +15,15 @@ class seosSeeder extends Seeder
     public function run()
     {
         Seos::create([
-
-
-
             'meta_author' => 'Admin',
             'meta_title' => 'admin@gmail.com',
             'meta_keyword' => 'Admin',
             'meta_description' => 'admin@gmail.com',
             'google_analytics' => 'Admin',
-            'google_analytics' => 'admin@gmail.com',
-            'google_verification' => 'Admin',
             'google_verification' => 'admin@gmail.com',
+            'alexa_analytics' => 'Admin',
+            'adsense_code' => 'Admin',
+            'fcmserver' => 'fcm Key',
 
         ]);
     }
