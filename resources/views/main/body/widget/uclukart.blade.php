a7c4b6edbb59bbb62fc1c977c5af76888056dcb3
obabeyi <43567012+obabeyi@users.noreply.github.com>
2022-10-22 15:09:50 +0300

    uclukart back
    uclukart back

--- resources/views/main/body/widget/uclukart.blade.php
+++ resources/views/main/body/widget/uclukart.blade.php
@@ -5,35 +5,29 @@
                 <h5 class="card-header kartlar__header bg-white">Gündem</h5>
 
                 <div class="position-relative">
-                    @if (isset($gundemcard[0]->title_tr))
-                        <a href="{{ URL::to('/haber-' . str_slug($gundemcard[0]->title_tr) . '-' . $gundemcard[0]->id) }}"
-                            class="kartlar__link">
-                            <img class="card-img-top position-relative rounded-0 lazyload img-fluid"
-                                src="{{ $gundemcard[0]->image }}"
-                                onerror="this.onerror=null;this.src='{{ $webSiteSetting->defaultImage }}';"
-                                alt="Card image cap">
-                            @if ($webSiteSetting->slider_title == 1)
-                                <div class="kartlar__effect position-absolute"></div>
-                            @endif
-                        </a>
-                    @endif
+                    <a href="{{ URL::to('/haber-' . str_slug($gundemcard[0]->title_tr) . '-' . $gundemcard[0]->id) }}"
+                        class="kartlar__link">
+                        <img class="card-img-top position-relative rounded-0 lazyload img-fluid"
+                            src="{{ $gundemcard[0]->image }}"
+                            onerror="this.onerror=null;this.src='{{ $webSiteSetting->defaultImage }}';"
+                            alt="Card image cap">
+                        @if ($webSiteSetting->slider_title == 1)
+                            <div class="kartlar__effect position-absolute"></div>
+                        @endif
+                    </a>
                 </div>
                 @if ($webSiteSetting->slider_title == 1)
                     <div class="card-body kartlar__body position-absolute" style="top:30%!important;">
                         <h5 class="card-title">
-                            @if (isset($gundemcard[0]->title_tr))
+                            {{ $gundemcard[0]->title_tr }}
 
-                                {{ $gundemcard[0]->title_tr }}
-                            @endif
                         </h5>
                         <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
                     </div>
                 @endif
                 <ul class="list-group list-group-flush">
-
                     @foreach ($gundemcard as $row)
-                        <a href="{{ URL::to('/haber-' . str_slug($row->title_tr) . '-' . $row->id) }}"
-                            class="kartlar__link">
+                        <a href="{{ URL::to('/haber-' . str_slug($row->title_tr) . '-' . $row->id) }}" class="kartlar__link">
                             <li class="list-group-item p-0">
                                 <img src="{{ asset($row->image) }}"
                                     onerror="this.onerror=null;this.src='{{ $webSiteSetting->defaultImage }}';"
@@ -51,34 +45,32 @@
         <div class="col-lg-4 col-md-6 col-sm-12 padding-left ">
             <div class="card font-weight-light shadow">
                 <h5 class="card-header kartlar__header bg-white">Siyaset</h5>
-                @if (isset($siyasetcard[0]->image))
-                    <div class="position-relative">
-                        <a href="{{ URL::to('/haber-' . str_slug($siyasetcard[0]->title_tr) . '-' . $siyasetcard[0]->id) }}"
-                            class="kartlar__link">
-                            <img class="card-img-top position-relative rounded-0 lazyload img-fluid"
-                                src="{{ $siyasetcard[0]->image }}"
-                                onerror="this.onerror=null;this.src='{{ $webSiteSetting->defaultImage }}';"
-                                alt="Card image cap">
-                            @if ($webSiteSetting->slider_title == 1)
-                                <div class="kartlar__effect position-absolute"></div>
-                            @endif
-                        </a>
-                    </div>
-                @endif
+
+                <div class="position-relative">
+                    <a href="{{ URL::to('/haber-' . str_slug($siyasetcard[0]->title_tr) . '-' . $siyasetcard[0]->id) }}"
+                        class="kartlar__link">
+                        <img class="card-img-top position-relative rounded-0 lazyload img-fluid"
+                            src="{{ $siyasetcard[0]->image }}"
+                            onerror="this.onerror=null;this.src='{{ $webSiteSetting->defaultImage }}';"
+                            alt="Card image cap">
+                        @if ($webSiteSetting->slider_title == 1)
+                            <div class="kartlar__effect position-absolute"></div>
+                        @endif
+                    </a>
+                </div>
                 @if ($webSiteSetting->slider_title == 1)
                     <div class="card-body kartlar__body position-absolute" style="top:30%!important;">
                         <h5 class="card-title">
-                            @if (isset($siyasetcard[0]->title_tr))
-                                {{ $siyasetcard[0]->title_tr }}
-                            @endif
+
+                            {{ $siyasetcard[0]->title_tr }}
+
                         </h5>
                         <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
                     </div>
                 @endif
                 <ul class="list-group list-group-flush">
                     @foreach ($siyasetcard as $row)
-                        <a href="{{ URL::to('/haber-' . str_slug($row->title_tr) . '-' . $row->id) }}"
-                            class="kartlar__link">
+                        <a href="{{ URL::to('/haber-' . str_slug($row->title_tr) . '-' . $row->id) }}" class="kartlar__link">
                             <li class="list-group-item p-0">
                                 <img src="{{ asset($row->image) }}"
                                     onerror="this.onerror=null;this.src='{{ $webSiteSetting->defaultImage }}';"
@@ -101,33 +93,30 @@
                 <h5 class="card-header kartlar__header bg-white">Ekonomi</h5>
 
                 <div class="position-relative">
-                    @if (isset($ekonomicard[0]->image))
-                        <a href="{{ URL::to('/haber-' . str_slug($ekonomicard[0]->title_tr) . '-' . $ekonomicard[0]->id) }}"
-                            class="kartlar__link">
-                            <img class="card-img-top position-relative rounded-0 lazyload img-fluid"
-                                src="{{ $ekonomicard[0]->image }}"
-                                onerror="this.onerror=null;this.src='{{ $webSiteSetting->defaultImage }}';"
-                                alt="Card image cap">
-                            @if ($webSiteSetting->slider_title == 1)
-                                <div class="kartlar__effect position-absolute"></div>
-                            @endif
-                        </a>
+                    <a href="{{ URL::to('/haber-' . str_slug($ekonomicard[0]->title_tr) . '-' . $ekonomicard[0]->id) }}"
+                        class="kartlar__link">
+                        <img class="card-img-top position-relative rounded-0 lazyload img-fluid"
+                            src="{{ $ekonomicard[0]->image }}"
+                            onerror="this.onerror=null;this.src='{{ $webSiteSetting->defaultImage }}';"
+                            alt="Card image cap">
+                        @if ($webSiteSetting->slider_title == 1)
+                            <div class="kartlar__effect position-absolute"></div>
+                        @endif
+                    </a>
                 </div>
                 @if ($webSiteSetting->slider_title == 1)
                     <div class="card-body kartlar__body position-absolute" style="top:30%!important;">
                         <h5 class="card-title">
-                            @if (isset($ekonomicard[0]->image))
 
-                                {{ $ekonomicard[0]->title_tr }}
-                            @endif
+                            {{ $ekonomicard[0]->title_tr }}
+
                         </h5>
                         <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
                     </div>
                 @endif
                 <ul class="list-group list-group-flush">
                     @foreach ($ekonomicard as $row)
-                        <a href="{{ URL::to('/haber-' . str_slug($row->title_tr) . '-' . $row->id) }}"
-                            class="kartlar__link">
+                        <a href="{{ URL::to('/haber-' . str_slug($row->title_tr) . '-' . $row->id) }}" class="kartlar__link">
                             <li class="list-group-item p-0">
                                 <img src="{{ asset($row->image) }}"
                                     onerror="this.onerror=null;this.src='{{ $webSiteSetting->defaultImage }}';"
