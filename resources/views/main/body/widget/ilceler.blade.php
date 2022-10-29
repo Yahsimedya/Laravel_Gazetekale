05f6ea55f33d862d2cdff6f0c537da3701b6b1a5
obabeyi <43567012+obabeyi@users.noreply.github.com>
2022-10-29 23:23:01 +0300

    ilçeler default image eklendi
    ilçeler default image eklendi

--- resources/views/main/body/widget/ilceler.blade.php
+++ resources/views/main/body/widget/ilceler.blade.php
@@ -5,15 +5,15 @@
         <div class="card-header float-left ilceler__baslik">İLÇE HABERLERİ</div>
         <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
             @php
-                $i=0;
+                $i = 0;
             @endphp
-            @foreach($ilceler as $ilce)
+            @foreach ($ilceler as $ilce)
                 <li class="nav-item ilceler__nav-item pr-0">
 
-                    <a class="ilceler__nav-link  @if($i==0)active @endif" id="pills-{{$ilce->id}}-tab"
-                       data-toggle="pill"
-                       href="#pills-{{$ilce->id}}" role="tab" aria-controls="pills-{{$ilce->id}}"
-                       aria-selected="false">{{$ilce->subdistrict_tr}}</a>
+                    <a class="ilceler__nav-link  @if ($i == 0) active @endif"
+                        id="pills-{{ $ilce->id }}-tab" data-toggle="pill" href="#pills-{{ $ilce->id }}"
+                        role="tab" aria-controls="pills-{{ $ilce->id }}"
+                        aria-selected="false">{{ $ilce->subdistrict_tr }}</a>
                 </li>
                 @php
                     $i++;
@@ -22,36 +22,37 @@
         </ul>
         <div class="tab-content " id="pills-tabContent">
 
-                @php
-                    $k=0;
-                @endphp
+            @php
+                $k = 0;
+            @endphp
 
-            @foreach($ilceler as $ilce)
-
-                <div class="tab-pane fade @if($k==0) show active @endif " id="pills-{{$ilce->id}}" role="tabpanel"
-                     aria-labelledby="pills-profile-tab">
+            @foreach ($ilceler as $ilce)
+                <div class="tab-pane fade @if ($k == 0) show active @endif "
+                    id="pills-{{ $ilce->id }}" role="tabpanel" aria-labelledby="pills-profile-tab">
 
 
                     <div class="row  padding-left">
                         <div class="col-md-12">
-                            <div class="ilcelers-{{$ilce->id}}">
+                            <div class="ilcelers-{{ $ilce->id }}">
                                 @php
-                                      $Ilcehaberleri= App\Models\Post::where('subdistrict_id', $ilce->id)->limit(8)->get();
+                                    $Ilcehaberleri = App\Models\Post::where('subdistrict_id', $ilce->id)
+                                        ->limit(8)
+                                        ->get();
                                 @endphp
-                                @foreach($Ilcehaberleri as $haber)
-                                    <a href="{{URL::to('/haber-'.str_slug($haber->title_tr).'-'.$haber->id)}}">
+                                @foreach ($Ilcehaberleri as $haber)
+                                    <a href="{{ URL::to('/haber-' . str_slug($haber->title_tr) . '-' . $haber->id) }}">
                                         <div class="card d-inline-block  ">
                                             <img class="card-img-top tns-lazy-img img-fluid"
-                                                 data-src="{{$haber->image}}">
+                                                onerror="this.onerror=null;this.src='image/logo/2021/12/61cae20554c80.61b9c9efc61e4.600801fbf2570-logo-yepyeni (2).png';"
+                                                data-src="{{ $haber->image }}">
                                             <div class="card-body align-middle d-table-cell">
                                                 <p class="card-baslik text-center d-table-cell"><b
-                                                        class="card-kisalt">{{$haber->title_tr}}</b></p>
+                                                        class="card-kisalt">{{ $haber->title_tr }}</b></p>
                                                 <span
-                                                    class="card__kategori position-absolute">{{$ilce->subdistrict_tr }}</span>
+                                                    class="card__kategori position-absolute">{{ $ilce->subdistrict_tr }}</span>
                                             </div>
                                         </div>
                                     </a>
-
                                 @endforeach
                             </div>
                         </div>
@@ -68,11 +69,11 @@
 
     </div>
 </section>
-@foreach($ilceler as $ilces)
+@foreach ($ilceler as $ilces)
     <script>
-        $(document).ready(function () {
+        $(document).ready(function() {
             tns({
-                container: ".ilcelers-{{$ilces->id}}",
+                container: ".ilcelers-{{ $ilces->id }}",
                 items: 3,
                 slideBy: "page",
                 nav: !0,
