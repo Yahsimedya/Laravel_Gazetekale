23a793361140263a810623496d06ca690e5f1423
obabeyi <43567012+obabeyi@users.noreply.github.com>
2022-11-26 17:03:40 +0300

    ++

--- resources/views/main/body/widget/anaslider.blade.php
+++ resources/views/main/body/widget/anaslider.blade.php
@@ -29,11 +29,12 @@
                 $k++;
             @endphp
         @endfor
-        <div class="item d-inline">
-            <span class="slider_span"><a href="#" class="mx-auto text-center align-middle text-white"><i
-                        class="fa fa-th-large"></i></a></span>
 
-        </div>
+
+    </div>
+    <div class="item d-inline">
+        <span class="slider_span"><a href="#" class="mx-auto text-center align-middle text-white"><i
+                    class="fa fa-th-large"></i></a></span>
 
     </div>
 </div>
