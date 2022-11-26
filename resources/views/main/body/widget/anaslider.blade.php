d9e17f95f768498528a193530ccb4322cce70716
obabeyi <43567012+obabeyi@users.noreply.github.com>
2022-11-26 16:48:04 +0300

    Update anaslider.blade.php

--- resources/views/main/body/widget/anaslider.blade.php
+++ resources/views/main/body/widget/anaslider.blade.php
@@ -31,11 +31,10 @@
     @endfor
 
 
-</div>
-<div class="item d-inline">
-    <span class="slider_span"><a href="#" class="mx-auto text-center align-middle text-white"><i
-                class="fa fa-th-large"></i></a></span>
+    <div class="item d-inline">
+        <span class="slider_span"><a href="#" class="mx-auto text-center align-middle text-white"><i
+                    class="fa fa-th-large"></i></a></span>
 
-</div>
+    </div>
 
 </div>
