f2941e6ab6801cfc772160832e51a77a2045a74c
obabeyi <43567012+obabeyi@users.noreply.github.com>
2022-12-17 00:41:30 +0300

    youtube video logo alanı güncellendi
    youtube video logo alanı güncellendi

--- resources/views/main/body/widget/youtube.blade.php
+++ resources/views/main/body/widget/youtube.blade.php
@@ -1,20 +1,20 @@
 <section class="video mt-3 pt-3 pb-3 bg-dark">
     <div class="container padding-left">
         <div class="row">
-            <div @if(count($egazete)>0 && $themeSetting->gazetesayisi==1) class="col-md-10" @else class="col-md-12"@endif >
+            <div @if (count($egazete) > 0 && $themeSetting->gazetesayisi == 1) class="col-md-10" @else class="col-md-12" @endif>
                 <div class="card-header card-youtube position-relative ">
                     <div class="d-flex justify-content-between">
                         <div class="col-md-9 col-8"><img class="img-fluid lazyload" width="180"
-                                                         data-src="{{asset('icon/gazetekaletv.png')}} ">
+                                data-src="{{ $webSiteSetting->video_logo }} ">
                             <div class="card-kutu__link">
                             </div>
                         </div>
                         <div class="col-md-3 col-4">
                             <div class="ytube">
-                                <img src="{{asset('icon/icon-youtube-white.svg')}}" class="lazyload img-fluid"
-                                     style="color: white" alt="youtube" width="75" height="21">
+                                <img src="{{ asset('icon/icon-youtube-white.svg') }}" class="lazyload img-fluid"
+                                    style="color: white" alt="youtube" width="75" height="21">
                                 <a href="https://www.youtube.com/channel/UCUUeGmpEvlPFjRSeOYevzag" target="_blank"
-                                   rel="nofollow" class="ytube-subscriber" title="Haberler.com Youtube">
+                                    rel="nofollow" class="ytube-subscriber" title="Haberler.com Youtube">
                                     <span>Abone Ol</span></a>
                             </div>
                         </div>
@@ -25,24 +25,24 @@
                         <div class="keskin">
                             @foreach ($youtube as $row)
                                 <div>
-                                    <a target="_blank" href="https://www.youtube.com/watch?v={{$row->posts_video}}">
+                                    <a target="_blank" href="https://www.youtube.com/watch?v={{ $row->posts_video }}">
                                         <div class="card   d-inline-block  ">
-                                            <img data-src="{{asset('icon/icon-youtube.png')}}"
-                                                 class="lazyload position-absolute ytube-icon img-fluid "
-                                                 alt="youtube" width="70" height="45">
+                                            <img data-src="{{ asset('icon/icon-youtube.png') }}"
+                                                class="lazyload position-absolute ytube-icon img-fluid " alt="youtube"
+                                                width="70" height="45">
                                             <img class="card-img-top tns-lazy-img lazyload img-fluid"
-                                                 data-src="{{$row->image}}"
-                                                 onerror="this.onerror=null;this.src='{{$webSiteSetting->defaultImage}}';"
-                                                 alt="{{$row->title_tr}}">
+                                                data-src="{{ $row->image }}"
+                                                onerror="this.onerror=null;this.src='{{ $webSiteSetting->defaultImage }}';"
+                                                alt="{{ $row->title_tr }}">
                                             <div class="card-body align-middle d-table-cell">
                                                 <p class="card-baslik text-center d-table-cell"><b
-                                                        class="card-kisalt">{{$row->title_tr}}</b></p>
-                                                <img data-src="{{asset('icon/icon-youtube.svg')}}"
-                                                     class="lazyload float-right img-fluid position-absolute"
-                                                     alt="youtube" width="75" height="21"
-                                                     style="top: 50%;right: 0;transform: translate(0%, 220%);">
+                                                        class="card-kisalt">{{ $row->title_tr }}</b></p>
+                                                <img data-src="{{ asset('icon/icon-youtube.svg') }}"
+                                                    class="lazyload float-right img-fluid position-absolute"
+                                                    alt="youtube" width="75" height="21"
+                                                    style="top: 50%;right: 0;transform: translate(0%, 220%);">
                                                 <span class="card__tv position-absolute text-dark">Gazetekale TV
-</span>
+                                                </span>
                                             </div>
                                         </div>
                                     </a>
@@ -52,21 +52,20 @@
                     </div>
                 </div>
             </div>
-            @if(count($egazete)>=1 && $themeSetting->gazetesayisi==1)
-                <div class="col-md-2"  >
+            @if (count($egazete) >= 1 && $themeSetting->gazetesayisi == 1)
+                <div class="col-md-2">
                     <div class="card-header card-egazete position-relative d-flex">
                         <h5 class="card-title text-light my-auto">E-Gazete</h5>
                     </div>
-                    <div class="e-gazete" >
-                        @foreach($egazete as $row)
+                    <div class="e-gazete">
+                        @foreach ($egazete as $row)
                             <div>
-                                <a class="example-image-link lb-number" target="_blank" href="{{$row->image}}"
-                                   data-lightbox="example-set" data-title="{{$row->title_tr}}">
-                                    <div class="card   d-inline-block  " >
+                                <a class="example-image-link lb-number" target="_blank" href="{{ $row->image }}"
+                                    data-lightbox="example-set" data-title="{{ $row->title_tr }}">
+                                    <div class="card   d-inline-block  ">
                                         <img class="card-img-top tns-lazy-img lazyload img-fluid example-image"
-                                             data-src="{{$row->image}}"
-                                             alt="{{$row->title_tr}}">
-                                        {{$row->title_tr}}
+                                            data-src="{{ $row->image }}" alt="{{ $row->title_tr }}">
+                                        {{ $row->title_tr }}
                                         <script>
                                             lightbox.option({
                                                 'albumLabel': "",
@@ -84,7 +83,7 @@
     </div>
 </section>
 <script>
-    $(document).ready(function () {
+    $(document).ready(function() {
         tns({
             container: ".keskin",
             items: 3,
@@ -117,7 +116,7 @@
     })
 </script>
 <script>
-    $(document).ready(function () {
+    $(document).ready(function() {
         tns({
             container: ".e-gazete",
             items: 3,
