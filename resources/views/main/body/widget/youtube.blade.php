b835dbf0e2e8060209a0bf01df7381d1941918a9
obabeyi <43567012+obabeyi@users.noreply.github.com>
2022-12-18 01:22:29 +0300

    youtube blade güncelleme
    youtube blade güncelleme

--- resources/views/main/body/widget/youtube.blade.php
+++ resources/views/main/body/widget/youtube.blade.php
@@ -14,7 +14,7 @@
                                 <img src="{{ asset('icon/icon-youtube-white.svg') }}" class="lazyload img-fluid"
                                     style="color: white" alt="youtube" width="75" height="21">
                                 <a href="{{ $webSiteSocial->youtube }}" target="_blank" rel="nofollow"
-                                    class="ytube-subscriber" title="Haberler.com Youtube">
+                                    class="ytube-subscriber" title="Youtube">
                                     <span>Abone Ol</span></a>
                             </div>
                         </div>
