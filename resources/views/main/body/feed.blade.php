cc1c18d4c2db106acb7bc4cbf79f1728f4ccbc36
obabeyi <43567012+obabeyi@users.noreply.github.com>
2023-08-22 02:38:28 +0300

    google news rss feed updated

--- resources/views/main/body/feed.blade.php
+++ resources/views/main/body/feed.blade.php
@@ -1,31 +1,23 @@
-<?=
-'<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL ?>
-<rss version="2.0">
+<?= '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL ?>
+<rss version="2.0" xmlns:media="http://search.yahoo.com/mrss/" xmlns:content="http://purl.org/rss/1.0/modules/content/">
     <channel>
         <title>{{ $seoset->meta_title }}</title>
-        <link>{{ url('/') }}></link>
+        <link>{{ url('/') }}</link>
         <description>{{ $seoset->meta_description }}</description>
         <language>tr</language>
         <pubDate>{{ now() }}</pubDate>
 
         @foreach ($posts as $post)
             <item>
-                <title>
-                    <![CDATA[{{ $post->title_tr }}]]>
-                </title>
+                <title><![CDATA[{{ $post->title_tr }}]]></title>
                 <link>{{ URL::to('/' . 'haber-' . str_slug($post->title_tr) . '-' . $post->id) }}</link>
-                <description>
-                    <![CDATA[{!! $post->description_tr !!}]]>
-                </description>
+                <description><![CDATA[{!! $post->description_tr !!}]]></description>
                 <category>{{ $post->category_tr }}</category>
-                <content>
-                    <![CDATA[{!! $post->details_tr !!}]]>
-                </content>
-                <author>{{ $seoset->meta_author }}></author>
+                <content:encoded><![CDATA[{!! $post->details_tr !!}]]></content:encoded>
+                <author>{{ $seoset->meta_author }}</author>
                 <guid>{{ URL::to('/' . 'haber-' . str_slug($post->title_tr) . '-' . $post->id) }}</guid>
                 <pubDate>{{ $post->created_at->toRssString() }}</pubDate>
-                <enclosure url="{{ URL::to('/' . $post->image) }}" type="image/jpeg" />
-
+                <media:content url="{{ URL::to('/' . $post->image) }}" type="image/jpeg" />
             </item>
         @endforeach
     </channel>
